b3ecf3e54208d207b5729c2a97a744bdae69db46
SupremeSith <woof.portocarrero@gmail.com>
2025-11-12 08:27:10 -0300

    Adicionados comentários descritivos em transacao.h para melhorar legibilidade e aderir às boas práticas de documentação

--- local-c/transacao.h
+++ local-c/transacao.h
@@ -1,16 +1,18 @@
 #ifndef TRANSACAO_H
 #define TRANSACAO_H
 
+// Estrutura que representa uma transação financeira
 typedef struct {
-    int id;
-    char tipo[20];
-    char categoria[50];
-    float valor;
-    char data[11];
+    int id;              // Identificador único da transação
+    char tipo[20];       // Tipo da transação (Receita ou Despesa)
+    char categoria[50];  // Categoria associada (ex: Salário, Aluguel)
+    float valor;         // Valor monetário da transação
+    char data[11];       // Data no formato DD/MM/AAAA
 } Transacao;
 
-void cadastrarTransacao();
-void listarTransacoes();
-int obterProximoID();
+// Declaração das funções relacionadas às transações
+void cadastrarTransacao();   // Cadastra uma nova transação
+void listarTransacoes();     // Lista todas as transações registradas
+int obterProximoID();        // Retorna o próximo ID disponível
 
 #endif
