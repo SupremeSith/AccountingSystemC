d575b7baab63c1f05714f6dd06237f61e2fd95df
SupremeSith <woof.portocarrero@gmail.com>
2025-11-12 08:26:18 -0300

    Adicionados comentários descritivos no arquivo utils.h para melhorar a legibilidade e seguir boas práticas de documentação do código

--- local-c/utils.h
+++ local-c/utils.h
@@ -1,24 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-
-
-
+// Define comando de limpeza de tela conforme o sistema operacional
 #ifdef _WIN32
     #define CLEAR "cls"
 #else
     #define CLEAR "clear"
 #endif
 
-
-
-void limparTela();
-void pausar();
-void criarDiretorioData();
-int validarData(char data[]);
-void desenharLinha(int tamanho);
-void desenharCabecalho(char *titulo);
-
-
+// Declaração de funções utilitárias do sistema
+void limparTela();                  // Limpa o terminal
+void pausar();                      // Pausa até o usuário pressionar ENTER
+void criarDiretorioData();          // Cria o diretório "data" se não existir
+int validarData(char data[]);       // Valida formato de data (DD/MM/AAAA)
+void desenharLinha(int tamanho);    // Desenha linha horizontal de tamanho definido
+void desenharCabecalho(char *titulo); // Exibe cabeçalho centralizado com bordas
 
 #endif
